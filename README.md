add34c3d3eb3a2ab43b1f3c231b175f065ef3bc4
PratikshaJain37 <pratikshajain123@gmail.com>
2020-06-21 23:33:32 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Documenting python scripts For lazy people:
+
+-> Document all functions. 
+-> Import all comments, and their line number
+
+
---
+++ lazy.py
@@ -0,0 +1,24 @@
+# Author: Pratiksha Jain
+# Created On: 21.06.20
+
+#-------------------------------#
+
+# For easy (lazy) documenting of python scripts #
+
+# File to be read
+name_of_file = 'ADAKNG'
+inputfile = open('%s.py'%s(name_of_file), 'r')
+
+# File to be put in 
+outputfile = open('Document.txt','a')
+
+
+outputfile.write('filler\n')
+
+
+
+
+
+
+outputfile.close()
+inputfile.close()
